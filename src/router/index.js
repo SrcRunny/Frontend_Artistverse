147d847fbca4b200a41e2cea08cff5a4c39af232
SrcRunny <Run34047@gmail.com>
2024-03-21 04:50:23 +0700

    end of set up today

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Homepage from '../views/Homepage.vue'
+import Homepage from '../views/HomepageView.vue'
+import Test from '../views/TestView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'Homepage',
       component: Homepage
+    },
+    {
+      path: '/test',
+      name: 'test',
+      component: Test
     }
   ]
 })
---
+++ src/views/HomepageView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>this is homepafe</h1>
+  </div>
+</template>
