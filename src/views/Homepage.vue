8c4801c6c03730d3900cb6627ab938cf1fa5c9d0
SrcRunny <Run34047@gmail.com>
2024-03-21 01:35:54 +0700

    Setup The Vue and library

---
+++ src/views/Homepage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>{{ msg }}</p>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:'TestView',
+        data(){
+            return{
+                msg:"hello this is test"
+            }
+        }
+    }
+</script>
