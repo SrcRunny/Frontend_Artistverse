fa24f07bda97e69b2132b23958b1621f4025073f
SrcRunny <Run34047@gmail.com>
2024-03-25 09:22:25 +0700

    create router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Homepage from '../views/HomepageView.vue'
-import Test from '../views/TestView.vue'
+import LyricsGenerator from '../views/LyricsGeneratorView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,9 +11,9 @@ const router = createRouter({
       component: Homepage
     },
     {
-      path: '/test',
-      name: 'test',
-      component: Test
+      path: '/generate-lyrics',
+      name: 'generate-lyrics',
+      component: LyricsGenerator
     }
   ]
 })
