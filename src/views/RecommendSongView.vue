d51590a19362f75a5eaa1dea0f600cfbf61ae23d
SrcRunny <Run34047@gmail.com>
2024-05-06 19:04:08 +0700

    can capture

--- src/views/RecommendSongView.vue
+++ src/views/RecommendSongView.vue
@@ -17,44 +17,44 @@
       <div style="margin: 10px; text-align: center; width: 49%">
         <img class="outer-shadow center img-fluid" :src="videoFeedURL" />
       </div>
+      <button @click="startCountdown" style="margin-top: 10px;">Capture Emotion</button>
+      <div v-if="showCountdown" style="font-size: 24px; color: red; margin-top: 10px;">{{ countdownNumber }}</div>
     </div>
 
     <div style="width: 50%; float: left; height: 100%; margin: auto; text-align: center">
       <h2 align="center" style="color: #ff3300">Song Recommendations</h2>
-    </div>
-
-    <div
-      class="outer-shadow"
-      id="ResultArea"
-      style="
-        padding: 15px;
-        width: 49%;
-        float: left;
-        height: 100%;
-        margin: auto;
-        text-align: center;
-        margin-bottom: 15px;
-      "
-    >
-      <table
-        class="table table-striped table-light table-bordered table-hover table-sm table-responsive"
-        id="DynamicTable"
+      <div
+        class="outer-shadow"
+        id="ResultArea"
+        style="
+          padding: 15px;
+          width: 100%;
+          height: 100%;
+          margin: auto;
+          text-align: center;
+          margin-bottom: 15px;
+        "
       >
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Album</th>
-            <th>Artist</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(item, index) in data" :key="index">
-            <td>{{ item.Name }}</td>
-            <td>{{ item.Album }}</td>
-            <td>{{ item.Artist }}</td>
-          </tr>
-        </tbody>
-      </table>
+        <table
+          class="table table-striped table-light table-bordered table-hover table-sm table-responsive"
+          id="DynamicTable"
+        >
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Album</th>
+              <th>Artist</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(item, index) in data" :key="index">
+              <td>{{ item.Name }}</td>
+              <td>{{ item.Album }}</td>
+              <td>{{ item.Artist }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
@@ -64,15 +64,36 @@ export default {
   data() {
     return {
       videoFeedURL: 'http://127.0.0.1:5000/video_feed',
-      data: []
+      data: [],
+      showCountdown: false,
+      countdownNumber: 0
     }
   },
-  mounted() {
-    setInterval(() => {
-      this.updateTable()
-    }, 100)
-  },
   methods: {
+    startCountdown() {
+      this.showCountdown = true;
+      let count = 3;
+      this.countdownNumber = count;
+      const countdownInterval = setInterval(() => {
+        if (count > 0) {
+          count--;
+          this.countdownNumber = count;
+        } else {
+          clearInterval(countdownInterval);
+          this.captureEmotion();
+        }
+      }, 1000);
+    },
+    captureEmotion() {
+      fetch('http://127.0.0.1:5000/video_feed')
+        .then(() => {
+          console.log('Emotion captured successfully!')
+          this.updateTable()
+        })
+        .catch((error) => {
+          console.error('Error capturing emotion:', error)
+        })
+    },
     updateTable() {
       fetch('http://127.0.0.1:5000/t')
         .then((response) => response.json())
